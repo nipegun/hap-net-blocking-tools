5d7c415b2c403f04d41bdd6b1291ea085335cb5f
NiPeGun <nipegun@gmail.com>
2020-04-15 05:45:15 +0200

    Update DeleteUselessFiles.sh

--- DeleteUselessFiles.sh
+++ DeleteUselessFiles.sh
@@ -10,7 +10,7 @@ ColorVerde='\033[1;32m'
 FinColor='\033[0m'
 
 echo ""
-echo -e "  ${ColorVerde}Deleting no longer needed files...${EndColor}" 
+echo -e "${ColorVerde}Deleting no longer needed files...${FinColor}" 
 echo ""
 
 # Remove the IPTables and NFTables sets

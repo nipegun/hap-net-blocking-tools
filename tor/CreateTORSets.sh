068e98ecdcf38208722c1cda390a790625965bb4
NiPeGun <nipegun@gmail.com>
2020-04-15 06:21:36 +0200

    Update and rename GetNodes.sh to CreateTORSets.sh

--- tor/CreateTORSets.sh
+++ tor/CreateTORSets.sh
@@ -9,6 +9,9 @@ ColorRojo='\033[1;31m'
 ColorVerde='\033[1;32m'
 FinColor='\033[0m'
 
+echo ""
+echo -e "${ColorVerde}  - Creating TOR HAProxy sets....${FinColor}"
+echo ""
 # Obtain WAN IP of the computer
 WANIP=$(curl --silent ipinfo.io/ip)
 # Create the HAProxy sets
@@ -17,9 +20,6 @@ wget -q https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=$WANIP -O - |
     sed -i '/^define TORNodes.ipv4 = {/a '"$IP"',' /root/scripts/hap-net-blocking-tools/tor/TORNodesIPv4.haproxy
     sed -i '/^define TORNodes.ipv6 = {/a '"$IP"',' /root/scripts/hap-net-blocking-tools/tor/TORNodesIPv6.haproxy
   done
-
-echo ""
-echo -e "${ColorVerde}  Creating TOR HAProxy sets....${FinColor}"
-echo ""
 find /root/scripts/hap-net-blocking-tools/tor/ -name "*.haproxy" -exec sed -i '1d' {} \;
 find /root/scripts/hap-net-blocking-tools/tor/ -name "*.haproxy" -exec sed -i 's/.$//' {} \;
+

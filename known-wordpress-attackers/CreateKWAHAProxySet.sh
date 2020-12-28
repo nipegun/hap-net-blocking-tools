57e51679a4cc60e2af2d605f02a480ba012b0b95
NiPeGun <nipegun@gmail.com>
2020-12-28 08:55:11 +0100

    Create CreateKWAHAProxySet.sh

---
+++ known-wordpress-attackers/CreateKWAHAProxySet.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Pongo a disposición pública este script bajo el término de "software de dominio público".
+# Puedes hacer lo que quieras con él porque es libre de verdad; no libre con condiciones como las licencias GNU y otras patrañas similares.
+# Si se te llena la boca hablando de libertad entonces hazlo realmente libre.
+# No tienes que aceptar ningún tipo de términos de uso o licencia para utilizarlo o modificarlo porque va sin CopyLeft.
+
+ColorRojo='\033[1;31m'
+ColorVerde='\033[1;32m'
+FinColor='\033[0m'
+
+echo ""
+echo -e "${ColorVerde}  - Creating Known Wordpress Attackers HAProxy sets....${FinColor}"
+echo ""
+# Obtain WAN IP of the computer
+WANIP=$(curl --silent ipinfo.io/ip)
+# Create the HAProxy sets
+truncate -s 0 /root/scripts/hap-net-blocking-tools/known-wordpress-attackers/KWA-IPv4.haproxy
+truncate -s 0 /root/scripts/hap-net-blocking-tools/known-wordpress-attackers/KWA-IPv6.haproxy
+echo 5.50.50.50 >> KWA-IPv4.haproxy
+
+# Borrar la primera línea del archivo
+find /root/scripts/hap-net-blocking-tools/known-wordpress-attackers/ -name "*.haproxy" -exec sed -i '1d' {} \;
+
+# Ordenar las IPs de forma ascendente
+sort -t . -k 1,1n /root/scripts/hap-net-blocking-tools/known-wordpress-attackers/KWA-IPv4.haproxy -o /root/scripts/hap-net-blocking-tools/known-wordpress-attackers/KWA-IPv4.haproxy
+
+#find /root/scripts/hap-net-blocking-tools/tor/ -name "*.haproxy" -exec sed -i 's/.$//' {} \;
+
